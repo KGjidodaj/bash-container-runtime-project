2443ebf251f89479c5082f2925beb91e19b9ad6d
KGjidodaj <90962093+KGjidodaj@users.noreply.github.com>
2026-06-19 19:04:16 +0300

    fix: implement shellcheck suggestions

--- test.sh
+++ test.sh
@@ -29,17 +29,19 @@ if [[ $command == "run" ]];then
     echo "This might take a few moments."
     sleep 0.6
 
-    $sudo_cmd docker export $($sudo_cmd docker create $image) -o "$image".tar.gz
+    $sudo_cmd docker export "$($sudo_cmd docker create "$image")" -o "$image".tar.gz
 
     mkdir -p workspace
     mv "$tar_file" workspace
-    cd workspace
+    cd workspace || exit
 
     $sudo_cmd tar --no-same-owner --no-same-permissions --owner=0 --group=0  -mxf "$tar_file"
     rm -f "$tar_file"
 
+  (
     cd ..
     $sudo_cmd chroot workspace /bin/bash
+  )
 
 fi
 
